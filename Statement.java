feaea34b1d34800d9dc7f527f4d63debf20521db
felipe-cerqerqueira-lyra <felipeclyra@gmail.com>
2022-02-02 20:34:23 +0000

    Commit 18

--- Statement.java
+++ Statement.java
@@ -1,3 +1,23 @@
-public class Statement {
+import java.util.Enumeration;
 
+public abstract class Statement {
+
+  public abstract String CustomerName(String Name);
+  public abstract String Owed(double Amount);
+  public abstract String Earned(int Amount);
+  public abstract String Figures(String Title, double charge);
+  
+  public String value(Customer aCustomer){
+    Enumeration rentals = aCustomer.getRentals();
+    String result = CustomerName(aCustomer.getName());
+    while (rentals.hasMoreElements()) {
+      Rental each = (Rental) rentals.nextElement();
+      //show figures for each rental
+      result += Figures(each.getMovie().getTitle(), each.getCharge());
+    }
+    //add footer lines
+    result += Owed(aCustomer.getTotalCharge());
+    result += Earned(aCustomer.getTotalFrequentRenterPoints());
+    return result;
+  }
 }
--- TextStatement.java
+++ TextStatement.java
@@ -1,19 +1,4 @@
-import java.util.Enumeration;
 public class TextStatement extends Statement {
-
-  public String value(Customer aCustomer){
-    Enumeration rentals = aCustomer.getRentals();
-    String result = CustomerName(aCustomer.getName());
-    while (rentals.hasMoreElements()) {
-      Rental each = (Rental) rentals.nextElement();
-      //show figures for each rental
-      result += Figures(each.getMovie().getTitle(), each.getCharge());
-    }
-    //add footer lines
-    result += Owed(aCustomer.getTotalCharge());
-    result += Earned(aCustomer.getTotalFrequentRenterPoints());
-    return result;
-  }
 
   public String CustomerName(String Name){
     return "Rental Record for " + Name + "\n";
