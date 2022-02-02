ec3844dc7a217037e84685abbe25dbdb359510ee
felipe-cerqueira-lyra <felipeclyra@gmail.com>
2022-02-02 19:14:51 +0000

    Commit 10

--- Movie.java
+++ Movie.java
@@ -24,23 +24,31 @@ public class Movie {
       return _title;
   }
 
-  public double getCharge (int daysRented){
+  public double getCharge(int daysRented){
     double thisAmount = 0;
     switch (getPriceCode()) {
-      case Movie.REGULAR:
+      case REGULAR:
         thisAmount += 2;
         if (daysRented > 2)
           thisAmount += (daysRented - 2) * 1.5;
         break;
-      case Movie.NEW_RELEASE:
+      case NEW_RELEASE:
         thisAmount += daysRented * 3;
         break;
-      case Movie.CHILDRENS:
+      case CHILDRENS:
         thisAmount += 1.5;
         if (daysRented > 3)
           thisAmount += (daysRented - 3) * 1.5;
         break;
     }
     return thisAmount;
+  }
+
+  public int getFrequentRenterPoints(int daysRented){
+    // add bonus for a two day new release rental
+    if ((getPriceCode() == NEW_RELEASE) && (daysRented > 1)) 
+      return 2;
+    else
+      return 1;
   }
 }
